e24940595787f5ed0a7d0f0646deba4e8cd64b7c
Zijian Han <hanzijian@live.com>
2019-07-18 05:50:29 -0500

    data with init

---
+++ script.js
@@ -0,0 +1,10 @@
+async function init() {
+	const overall = await d3.csv("data/overall.csv");
+	const data1995 = await d3.csv("data/1995.csv");
+	const data2000 = await d3.csv("data/2000.csv");
+	const data2005 = await d3.csv("data/2005.csv");
+	const data2010 = await d3.csv("data/2010.csv");
+	const data2015 = await d3.csv("data/2015.csv");
+	const data2017 = await d3.csv("data/2017.csv");
+	console.log(data1995);
+}
