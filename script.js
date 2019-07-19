37975e03ab78b1d842e7802bf0a0c77b793d62e3
Zijian Han <hanzijian@live.com>
2019-07-18 20:34:57 -0500

    formatted data init

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function init() {
 	const overall = await d3.csv("data/overall.csv");
-	const data1995 = await d3.csv("data/1995.csv");
 	const data2000 = await d3.csv("data/2000.csv");
 	const data2005 = await d3.csv("data/2005.csv");
 	const data2010 = await d3.csv("data/2010.csv");
