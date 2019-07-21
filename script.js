8bc64c7bb327492fd6d121ef8b4e454ae60dd2b2
Zijian Han <hanzijian@live.com>
2019-07-21 14:09:33 -0500

    update data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var x = d3.scaleLinear().domain([0, 100]).range([0, 400]);
 var slide = 1;
 var overall = [];
+var div;
 		
 async function init() {
 	var data2000 = await d3.csv("data/2000.csv");
@@ -15,7 +16,7 @@ async function init() {
 	overall.push(data2015);
 	overall.push(data2017);
 	
-	var div = d3.select("body").append("div")	
+	div = d3.select("body").append("div")	
     .attr("class", "tooltip")				
     .style("opacity", 0);
 	
@@ -43,18 +44,32 @@ function draw(n) {
                 .style("opacity", 0);	
         });
 	
+	var i;
+	var items = [];
+	for (i = 1; i <= 10; i++) { 
+		items.push(overall[n-1][i-1]);
+	}
+	
+	var y = d3.scaleOrdinal().domain(items).range([0, 350]);
+	
+	d3.select(".chart").append("g").attr("transform", "translate(0, 0)").call(d3.axisLeft(y));
 	d3.select(".chart").append("g").attr("transform", "translate(70, 400)").call(d3.axisBottom(x));
 	d3.select(".chart").append("text").attr("x", 220).attr("y", 460).text("Arrivals (Units in millions)");
-	d3.select(".chart").append("text").data(overall[n-1]).text(function(d) {return d.Country;}).attr("x", 0).attr("y", function(d, i) {return i * 35;});
+	//d3.select(".chart").append("text").data(overall[n-1]).text(function(d) {return d.Country;}).attr("x", 0).attr("y", function(d, i) {return i * 35;});
 	
 	svg.append("rect").attr("x", 300).attr("y", 250).attr("width", 10).attr("height", 10).style("fill", "#4C5270")
 	svg.append("rect").attr("x", 300).attr("y", 270).attr("width", 10).attr("height", 10).style("fill", "#F652A0")
 	svg.append("rect").attr("x", 300).attr("y", 290).attr("width", 10).attr("height", 10).style("fill", "#36EEE0")
 	svg.append("rect").attr("x", 300).attr("y", 310).attr("width", 10).attr("height", 10).style("fill", "#BCECE0")
-	svg.append("text").attr("x", 315).attr("y", 270).text("Europe & Central Asia").style("font-size", "15px");
-	svg.append("text").attr("x", 315).attr("y", 290).text("North America").style("font-size", "15px");
-	svg.append("text").attr("x", 315).attr("y", 310).text("East Asia & Pacific").style("font-size", "15px");
-	svg.append("text").attr("x", 315).attr("y", 330).text("Latin America & Caribbean").style("font-size", "15px");
+	svg.append("text").attr("x", 315).attr("y", 260).text("Europe & Central Asia").style("font-size", "15px");
+	svg.append("text").attr("x", 315).attr("y", 280).text("North America").style("font-size", "15px");
+	svg.append("text").attr("x", 315).attr("y", 300).text("East Asia & Pacific").style("font-size", "15px");
+	svg.append("text").attr("x", 315).attr("y", 320).text("Latin America & Caribbean").style("font-size", "15px");
+	
+	var year = 2015;
+	if (n == 2) year = 2016;
+	if (n == 3) year = 2017;
+	svg.append("text").attr("x", 100).attr("y", 5).text("Top 10 Visited Countries in " + year).style("font-size", "32px");
 }
 			
 function set(n) {
