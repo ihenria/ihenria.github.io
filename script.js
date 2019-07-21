8873bfd535142547542205ad78639baaa4bf7cd3
Zijian Han <hanzijian@live.com>
2019-07-20 20:37:34 -0500

    update data

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-async function init() {
-	const overall = await d3.csv("data/overall.csv");
-	const data2000 = await d3.csv("data/2000.csv");
-	const data2005 = await d3.csv("data/2005.csv");
-	const data2010 = await d3.csv("data/2010.csv");
-	const data2015 = await d3.csv("data/2015.csv");
-	const data2017 = await d3.csv("data/2017.csv");
-	console.log(data2000);
-	
-	var x = d3.scaleLinear().domain([0, 100000000]).range([0, 300]);
-	d3.select(".chart").selectAll("rect").data(data2000).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);})
-	.attr("height", 30).attr("y", function(d, i) {return i * 40;});
-}
+var x = d3.scaleLinear().domain([0, 100000000]).range([0, 300]);
 
+var data2010, data2015, data2010, data2015, data2017, overall;
+var svg = d3.select(".chart").append("svg")
+            .attr("width", 400)
+            .attr("height", 600)
+            .append("g")
+            .attr("transform", "translate(" + 50 + "," + 50 + ")");
+			
+async function init() {
+	overall = await d3.csv("data/overall.csv");
+	data2000 = await d3.csv("data/2000.csv");
+	data2005 = await d3.csv("data/2005.csv");
+	data2010 = await d3.csv("data/2010.csv");
+	data2015 = await d3.csv("data/2015.csv");
+	data2017 = await d3.csv("data/2017.csv");
+	
+	svg.selectAll("rect").data(data2000).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);})
+	.attr("height", 20).attr("y", function(d, i) {return i * 30;});
+}
+			
 function set(n) {
 	d3.select(".chart").html = "";
 	if (n == 1) {
-		d3.select(".chart").selectAll("rect").data(data2000).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2000).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 	
 	if (n == 2) {
-		d3.select(".chart").selectAll("rect").data(data2000).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2000).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 	
 	if (n == 3) {
-		d3.select(".chart").selectAll("rect").data(data2005).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2005).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);}).attr("height", 20).attr("y", function(d, i) {return i * 30;});
 	}
 	
 	if (n == 4) {
-		d3.select(".chart").selectAll("rect").data(data2010).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2010).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);}).attr("height", 20).attr("y", function(d, i) {return i * 30;});
 	}
 	
 	if (n == 5) {
-		d3.select(".chart").selectAll("rect").data(data2015).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2015).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 	
 	if (n == 6) {
-		d3.select(".chart").selectAll("rect").data(data2017).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
+		svg.selectAll("rect").data(data2017).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 }
