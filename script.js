5f707c0672e702b1fb5fba9f1c6131ff3a3bdf81
Zijian Han <hanzijian@live.com>
2019-07-21 08:42:28 -0500

    update data

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var x = d3.scaleLinear().domain([0, 100]).range([0, 400]);
 var slide = 1;
 
-var data2010, data2015, data2010, data2015, data2017, overall;
+var data2010, data2015, data2010, data2015, data2017;
 
 var div = d3.select("body").append("div")	
     .attr("class", "tooltip")				
     .style("opacity", 0);
 		
 async function init() {
-	overall = await d3.csv("data/overall.csv");
 	data2000 = await d3.csv("data/2000.csv");
 	data2005 = await d3.csv("data/2005.csv");
 	data2010 = await d3.csv("data/2010.csv");
@@ -36,7 +35,7 @@ async function init() {
         });
 	
 	var items = [], i;
-	for (i = 0; i < 20; i++) { 
+	for (i = 0; i < 10; i++) { 
 		items.push(data2000[i].Country);
 	}
 	var y = d3.scaleOrdinal().domain(items).range([0, 500]);
@@ -56,67 +55,35 @@ function set(n) {
 	slide = n;
 	d3.select(".chart").html("");
 	var i;
-	for (i = 1; i <= 6; i++) { 
+	for (i = 1; i <= 5; i++) { 
 		d3.select("#btn" + i).attr("class", "");
 	}
 	
 	d3.select("#btn" + n).attr("class", "active");
+	
 	if (n == 1) {
-		var valueline = d3.line()
-		.x(function(d) { return x(d.Year); })
-		.y(function(d) { return y(d.Arrivals); });
-			
-		var svg = d3.select(".chart").append("svg")
-		.attr("width", 500)
-		.attr("height", 450)
-		.append("g")
-		.attr("transform", "translate(" + 50 + "," + 50 + ")");
-		
-		svg.append("path").data(overall).attr("class", "line").attr("d", valueline).on("mouseover", function(d) {
-				div.transition()		
-					.duration(200)		
-					.style("opacity", .9);		
-				div.html("Year: " + d.Year + "<br/>"  + "Arrivals: " + d.Arrivals)	
-					.style("left", (d3.event.pageX) + "px")		
-					.style("top", (d3.event.pageY - 28) + "px");	
-				})					
-			.on("mouseout", function(d) {		
-				div.transition()		
-					.duration(500)		
-					.style("opacity", 0);	
-			});
-		
-		var x = d3.scaleLinear().domain([2000, 2017]).range([0, 400]);
-		var y = d3.scaleLinear().domain([0, 1400]).range([0, 500]);
-		
-		d3.select(".chart").append("g").attr("transform", "translate(50, 650)").call(d3.axisBottom(x));
-		d3.select(".chart").append("g").attr("transform", "translate(0, 0)").call(d3.axisLeft(y));
-	
-	}
-	
-	if (n == 2) {
 		d3.select(".chart").selectAll("rect").data(data2000).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 	
-	if (n == 3) {
+	if (n == 2) {
 		d3.select(".chart").selectAll("rect").data(data2005).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);}).attr("height", 20).attr("y", function(d, i) {return i * 30;});
 	}
 	
-	if (n == 4) {
+	if (n == 3) {
 		d3.select(".chart").selectAll("rect").data(data2010).enter().append("rect").attr("class", "bar").attr("width", function(d){return x(d.Arrivals);}).attr("height", 20).attr("y", function(d, i) {return i * 30;});
 	}
 	
-	if (n == 5) {
+	if (n == 4) {
 		d3.select(".chart").selectAll("rect").data(data2015).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 	
-	if (n == 6) {
+	if (n == 5) {
 		d3.select(".chart").selectAll("rect").data(data2017).enter().append("rect").attr("width", 19).attr("height", function(d){return d.Arrivals;});
 	}
 }
 
 function next() {
-	if (slide < 6) {
+	if (slide < 5) {
 		slide++;
 		set(slide);
 	}
